c74e4f12d2d140d715cd5aba53aebd37b19d556e
Ritesh Kumar <94539726+Ritesh115@users.noreply.github.com>
2025-11-15 14:53:57 +0000

    commit

--- laravel/first.php
+++ laravel/first.php
@@ -25,18 +25,69 @@ frameworks are pre written codes and libraries which help us to build web applic
  1. check for XAMPP
 cd /opt/lampp
 ls
-
 <!--  -->
-sudo service apache2 stop
-sudo /opt/lampp/lampp startapache
+sudo service apache2 stop            //Apache => it is a web srevre 
+sudo /opt/lampp/lampp startapache            // a local server is created on system .
 
 sudo service mysql stop
 sudo /opt/lampp/lampp startmysql
 
+
+Composer is a dependency manager for PHP. It allows you to declare the libraries your project needs and installs them for you.
+
 <!-- project setup -->
- composer create-project laravel/laravel myapp "11."
+ composer create-project laravel/laravel project_name "11."
 
  2.start the local dev server 
  php artisan server
- 3.
 
+3. open the project in vs code .
+
+4. Database configuration 
+ open .env file in root directory of project 
+ <!-- Make sure you create the database manually in phpMyAdmin or MySQL Workbench. -->
+ 
+ 5.use migration to create tables in DB
+ => php artisan migrate 
+ when you run this command laravel will check the migrations folder in database folder and create the tables in DB as per the code written in migration files .
+
+6. you can create your own migration file using command 
+ => php artisan make:migration create_table_name_table
+
+7. Create model with migration
+=> php artisan make:model nameofmodel 
+ this command will create a model file in app/models folder and migration file in database/migrations folder 
+
+# with php artisan migrate:status   #we can check the status of migration
+
+# Run all pending migrations
+php artisan migrate
+
+8. MVC flow 
+controller 
+  => php artisan make:controller ControllerName 
+  this command will create a controller file in app/http/controllers folder
+
+Routes setup 
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+
+View setup 
+  => create a folder in resources/views folder with name same as controller name in lowercase 
+  => create a blade file with name same as function name in controller file with .blade.php extension
+
+<!-- end -->
+
+
+
+
+
+
+
+
+
+
+<!-- to check if db is connected or not -->
+ php artisan migrate:status
